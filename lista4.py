1ddf3cf82313b9f066f2621701b047a87828af9d
Isa60904 <isavalentimfrancisco@gmail.com>
2024-06-14 22:06:15 -0300

    Update lista4.py

--- lista4.py
+++ lista4.py
@@ -11,12 +11,12 @@ st.set_page_config(
 st.subheader("Exercício Projetos")
 
 code = '''
-df = pd.read_csv("https://raw.githubusercontent.com/isa60904/Prova-4/main/projetos.csv", sep=";") 
+df = pd.read_csv("https://raw.githubusercontent.com/Isa60904/Prova-4-/main/projetos.csv", sep=";") 
 st.dataframe(df.head(23))
 '''
 st.code(code, language='python')
 
-df = pd.read_csv("https://raw.githubusercontent.com/isa60904/Prova-4/main/projetos.csv", sep=";") 
+df = pd.read_csv("https://raw.githubusercontent.com/Isa60904/Prova-4-/main/projetos.csv", sep=";") 
 st.dataframe(df.head(23))
 
 st.write("O dataframe foi atualizado adicionando mais uma linha ao final com os dados referentes ao mês de dezembro de 2023.")
